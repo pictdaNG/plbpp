cb0b0af2570103865a8c63f4076726858610559d
Nanichang <nanipaul68@gmail.com>
2019-04-03 21:20:06 +0100

    added active class to navbar

--- src/Components/Common/NavBar.js
+++ src/Components/Common/NavBar.js
@@ -3,21 +3,23 @@ import { Link } from 'react-router-dom';
 
 export default class NavBar extends Component {
   render() {
+    
+    let here = window.location.pathname;
     return (
       <div data-uk-sticky="show-on-up: true; animation: uk-animation-slide-top;">
         <nav className="uk-navbar-container" data-uk-navbar style={{position: 'relative', zIndex: '980'}}>
           <div className="main-container">
             <div className="uk-navbar-left">
               <ul className="uk-navbar-nav">
-                <li className="uk-active"><Link to="/" >Home</Link></li>
-                <li ><Link to="/about">About Us</Link></li>
-                {/* <li ><Link to="/documents">Documents</Link></li> */}
-                {/* <li ><Link to="/tender">Tenders</Link></li> */}
-                <li ><Link to="/e-procurement">e-Procurement</Link></li>
-                {/* <li ><Link to="/open-contracting">Open Contracting</Link></li> */}
-                {/* <li ><Link to="/media">Media</Link></li> */}
-                {/* <li ><Link to="/">More</Link></li> */}
-                {/* <li ><Link to="/contact">Contact</Link></li> */}
+                <li className={here == '/' ? "uk-active" : null}><Link to="/" >Home</Link></li>
+                <li className={here == '/about' ? "uk-active" : null}><Link to="/about">About Us</Link></li>
+                {/* <li className={here == '/' ? "uk-active" : null}><Link to="/documents">Documents</Link></li> */}
+                {/* <li className={here == '/' ? "uk-active" : null}><Link to="/tender">Tenders</Link></li> */}
+                <li className={here == '/e-procurement' ? "uk-active" : null}><Link to="/e-procurement">e-Procurement</Link></li>
+                {/* <li className={here == '/' ? "uk-active" : null}><Link to="/open-contracting">Open Contracting</Link></li> */}
+                {/* <li className={here == '/' ? "uk-active" : null}><Link to="/media">Media</Link></li> */}
+                {/* <li className={here == '/' ? "uk-active" : null}><Link to="/">More</Link></li> */}
+                {/* <li className={here == '/' ? "uk-active" : null}><Link to="/contact">Contact</Link></li> */}
               </ul>
               <Link className="uk-navbar-toggle" data-uk-navbar-toggle-icon to="#"></Link>
             </div>
