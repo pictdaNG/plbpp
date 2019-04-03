191a0c456080907aa3e47baa899f3b27bf0795d5
daaef <realafe21@gmail.com>
2019-04-03 22:19:34 +0100

    changes to navigations

--- src/Components/Common/NavBar.js
+++ src/Components/Common/NavBar.js
@@ -13,13 +13,13 @@ export default class NavBar extends Component {
               <ul className="uk-navbar-nav">
                 <li><NavLink activeClassName="uk-active"exact={true}  to="/">Home</NavLink></li>
                 <li><NavLink activeClassName="uk-active"exact={true}  to="/about">About Us</NavLink></li>
-                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Documents (Downloads)</NavLink></li>
-                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Tenders</NavLink></li>
+               {/* <li><NavLink activeClassName="uk-active"exact={true}  to="#">Documents (Downloads)</NavLink></li>
+                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Tenders</NavLink></li>*/}
                 <li><NavLink activeClassName="uk-active"exact={true}  to="/mandate">Mandate</NavLink></li>
                 <li><NavLink activeClassName="uk-active"exact={true}  to="/e-procurement">E-Procurement</NavLink></li>
-                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Media</NavLink></li>
+                {/*<li><NavLink activeClassName="uk-active"exact={true}  to="#">Media</NavLink></li>
                 <li><NavLink activeClassName="uk-active"exact={true}  to="#">More</NavLink></li>
-                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Contact</NavLink></li>
+                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Contact</NavLink></li>*/}
               </ul>
               <button className="uk-navbar-toggle" data- uk-toggle="target: #sidenav" data-uk-icon="icon: menu" />
             </div>
--- src/Components/Common/SideNav.js
+++ src/Components/Common/SideNav.js
@@ -7,15 +7,15 @@ export default class SideNav extends Component {
     <div id="sidenav" data-uk-offcanvas="overlay: true">
       <div className="uk-offcanvas-bar uk-flex uk-flex-column">
         <ul className="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
-          <li><NavLink activeClassName="uk-active"exact={true}  to="/">Home</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="/about">About Us</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="#">Documents (Downloads)</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="#">Tenders</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="/mandate">Mandate</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="/e-procurement">E-Procurement</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="#">Media</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="#">More</NavLink></li>
-          <li><NavLink activeClassName="uk-active"exact={true}  to="#">Contact</NavLink></li>
+          <li><NavLink activeClassName="uk-active" exact={true}  to="/">Home</NavLink></li>
+          <li><NavLink activeClassName="uk-active" exact={true}  to="/about">About Us</NavLink></li>
+          {/* <li><NavLink activeClassName="uk-active"exact={true}  to="#">Documents (Downloads)</NavLink></li>
+                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Tenders</NavLink></li>*/}
+          <li><NavLink activeClassName="uk-active" exact={true}  to="/mandate">Mandate</NavLink></li>
+          <li><NavLink activeClassName="uk-active" exact={true}  to="/e-procurement">E-Procurement</NavLink></li>
+          {/*<li><NavLink activeClassName="uk-active"exact={true}  to="#">Media</NavLink></li>
+                <li><NavLink activeClassName="uk-active"exact={true}  to="#">More</NavLink></li>
+                <li><NavLink activeClassName="uk-active"exact={true}  to="#">Contact</NavLink></li>*/}
         </ul>
       </div>
     </div>
