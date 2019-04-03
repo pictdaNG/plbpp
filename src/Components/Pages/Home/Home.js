8aeb51af6d22c2eadd84f876ef66f698590debd9
Nanichang <nanipaul68@gmail.com>
2019-04-03 16:58:16 +0100

    added about content

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -130,13 +130,15 @@ export default class Home extends Component {
           <div className="about mt:20px">
             <div className="info">
               <h3 className="title">ABOUT <span className="theme-color uk-text-bold">PLPPA</span></h3>
+
               <div className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" data-uk-grid>
                 <div className="uk-card-media-left media uk-cover-container">
                   <img src="./assets/img/lally.png" alt="" />
                 </div>
                 <div className="uk-flex uk-flex-middle">
                   <div className="uk-card-body">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cum, dolor iusto quisquam quos rem. At eos eum ipsum quidem sed suscipit vitae. Itaque, qui, tenetur? Cupiditate dolor ipsam perferendis?</p>
+                    <p>The wind of change in public procurement that guarantees transparency, accountability, competition and cost effectiveness was imbibed by the Plateau State Government.</p>
+                    <p> A Law that seeks to bring a reverse in Public procurement process was considered by the Plateau State House of assembly.</p>
                   </div>
                 </div>
               </div>
@@ -146,7 +148,9 @@ export default class Home extends Component {
                 </div>
                 <div className="uk-flex uk-flex-middle">
                   <div className="uk-card-body">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae corporis cupiditate facilis in iusto laudantium mollitia officia provident quae quod rem ut velit voluptas, voluptatibus? Dolor neque non unde!</p>
+                    <p>On December 17, 2015 a law was passed by the State Assembly as Plateau State Bureau of Public Procurement Bill. It was signed into Law by the Plateau state Governor, Rt. Hon. Simon Bako Lalong on the 1st February, 2016. Plateau State keyed in to the International best practice in public procurement.</p>
+                    
+                    <p>Bldr. Peter Y. Dogo, the pioneer Director General was appointed by the Executive Governor and confirmed by the House of assembly on 14th February, 2017. This appointment and subsequent inauguration kick-started the activities of the Bureau of Public Procurement in Plateau State.</p>
                   </div>
                 </div>
               </div>
